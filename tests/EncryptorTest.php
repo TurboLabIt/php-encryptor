cf7cf10f261ffe4b7d57b6808d45e48e35e10624
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-03-25 22:51:34 +0100

    Create EncryptorTest.php

---
+++ tests/EncryptorTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace TurboLabIt\Encryptor\tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+class ForeachableTest extends TestCase
+{
+    public function testBase(): void
+    {
+
+    }
+}
