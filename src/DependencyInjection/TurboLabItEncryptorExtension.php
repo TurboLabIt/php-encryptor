662688d865eb712a632a9af355a49114e8f3095b
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2022-04-26 10:35:25 +0200

    Symfony Bundle

--- src/DependencyInjection/TurboLabItEncryptorExtension.php
+++ src/DependencyInjection/TurboLabItEncryptorExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class TurboLabItEncryptorExtension extends Extension
 {
-    protected function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
         $loader->load('services.yaml');
