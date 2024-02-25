f69bb3ab1ff35c452e742af176f5bb59c3e4eb2c
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-26 00:28:54 +0100

    AbstractBundle is incompatible with DependencyInjection/TurboLabItEncryptorExtension => services.yaml must be loaded inside the main bundle definition itself

--- src/TurboLabItEncryptorBundle.php
+++ src/TurboLabItEncryptorBundle.php
@@ -2,6 +2,14 @@
 namespace TurboLabIt\Encryptor;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 
-class TurboLabItEncryptorBundle extends AbstractBundle {}
+class TurboLabItEncryptorBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+}
